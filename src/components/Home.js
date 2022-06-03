3e038d2052b88896fba2b2c66c72ec65ab39853e
Kyaw Thet <kyawthet.2017.flc@gmail.com>
2022-06-03 10:22:21 +0630

    Revert "Revert "Hidden Dot""
    This reverts commit 089b2be46f7d3730b77a20ecb8daa4e4ded36901.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,19 @@
 import React from "react";
 
+import { useMoralisQuery } from "react-moralis";
+
 const Home = () => {
+
+    // const collectionName = "alphabet";    
+    // const { data: collections } = useMoralisQuery("CvlCollections", (query) =>
+    //     query.fullText("name", collectionName)
+    // );
+    const { data: collections } = useMoralisQuery("CvlCollections", (query) =>
+        query.equalTo("objectId", "bdJhoxg2dXAAbff6IBtx9JOH")
+    );
+
+    console.log("collections: ", collections)
+
     return(<h3>Home Page</h3>);
 }
 
