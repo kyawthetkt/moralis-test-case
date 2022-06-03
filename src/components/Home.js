9ce3f03d502050d9d6960ea884bf601e58363e0a
Kyaw Thet <kyawthet.2017.flc@gmail.com>
2022-06-03 11:16:54 +0630

    Add Databsae Integration

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-import { useMoralisQuery, useNewMoralisObject } from "react-moralis";
+import { useMoralisQuery } from "react-moralis";
 
 const Home = () => {
-
-    const { save } = useNewMoralisObject("testCollection");
     
     // Even though there is record related to bdJhoxg2dXAAbff6IBtx9JOH, no data is returned.
     const { data: collections } = useMoralisQuery("collection", (query) =>
