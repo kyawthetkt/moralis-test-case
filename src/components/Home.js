db1bab9fa0d656b3be3f592184e4f3396b7a9e16
Kyaw Thet <kyawthet.2017.flc@gmail.com>
2022-06-03 10:29:09 +0630

    Update Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,12 @@ import React from "react";
 import { useMoralisQuery } from "react-moralis";
 
 const Home = () => {
-
-    // const collectionName = "alphabet";    
+ 
     // const { data: collections } = useMoralisQuery("CvlCollections", (query) =>
-    //     query.fullText("name", collectionName)
+    //     query.equalTo("objectId", "bdJhoxg2dXAAbff6IBtx9JOH")
     // );
-    const { data: collections } = useMoralisQuery("CvlCollections", (query) =>
-        query.equalTo("objectId", "bdJhoxg2dXAAbff6IBtx9JOH")
-    );
 
-    console.log("collections: ", collections)
+    // console.log("collections: ", collections)
 
     return(<h3>Home Page</h3>);
 }
